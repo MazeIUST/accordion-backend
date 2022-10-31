ee3ded2d3c14c8089b40268f22522cef0c217504
mhhasani <hasanimohammadhosein@gmail.com>
2022-10-31 10:43:53 +0330

    signup

--- accordion/accounts/urls.py
+++ accordion/accounts/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('signup/', signup, name='signup'),
 ]
--- accordion/accounts/views.py
+++ accordion/accounts/views.py
@@ -1,4 +1,8 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
@@ -14,3 +18,21 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     serializer_class = UserSerializer
     queryset = get_user_model().objects.all()
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        data = request.POST   
+        print(data)         
+        new_user = {
+            'username': data.get('username'),
+            'password': data.get('password'),
+            'email': data.get('email'),
+            'first_name': data.get('first_name'),
+            'last_name': data.get('last_name'),
+        }
+        user = User(username=new_user['username'], email=new_user['email'], first_name=new_user['first_name'], last_name=new_user['last_name'])
+        user.save()
+        return JsonResponse({'status': 'User created successfully'})
+    return JsonResponse({'status': 'Get Method not allowed'})
