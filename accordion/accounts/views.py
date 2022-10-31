43290e8570a233f8876b6b1643d73a2e9e8d2455
mhhasani <hasanimohammadhosein@gmail.com>
2022-10-31 11:01:42 +0330

    cleaning code

--- accordion/accounts/views.py
+++ accordion/accounts/views.py
@@ -2,6 +2,10 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
 from .serializers import UserSerializer, RegisterSerializer
+from django.contrib.auth import login
+from rest_framework import permissions
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
 
 # Register API
 class RegisterAPI(generics.GenericAPIView):
@@ -12,14 +16,11 @@ class RegisterAPI(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         return Response({
-        "user": UserSerializer(user, context=self.get_serializer_context()).data,
-        "token": AuthToken.objects.create(user)[1]
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
         })
 
-from django.contrib.auth import login
-from rest_framework import permissions
-from rest_framework.authtoken.serializers import AuthTokenSerializer
-from knox.views import LoginView as KnoxLoginView
+
 
 class LoginAPI(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
