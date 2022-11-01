8f2ba2febaee5d6ffc9f22493c31007a64f853f2
mhhasani <hasanimohammadhosein@gmail.com>
2022-11-01 13:18:07 +0330

    EMAIL_HOST_PASSWORD

--- accordion/accordion/settings.py
+++ accordion/accordion/settings.py
@@ -157,4 +157,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hasanimohammadhosein@gmail.com'
-EMAIL_HOST_PASSWORD = '*********'
+EMAIL_HOST_PASSWORD = 'qmhzfspdbtjdrvjc'
