07eedd17ce5c66ace28e1ff0d4857e728f96584b
mhhasani <hasanimohammadhosein@gmail.com>
2022-11-01 18:58:29 +0330

    'email': {'unique': True}

--- accordion/accounts/serializers.py
+++ accordion/accounts/serializers.py
@@ -12,7 +12,10 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'unique': True}
+        }
 
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
