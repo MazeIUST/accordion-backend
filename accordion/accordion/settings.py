35e8597e38abdc49207e40ba1486f1bdfae970ba
mhhasani <hasanimohammadhosein@gmail.com>
2022-11-01 18:42:25 +0330

    EMAIL_HOST_USER

--- accordion/accordion/settings.py
+++ accordion/accordion/settings.py
@@ -156,5 +156,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'hasanimohammadhosein@gmail.com'
-EMAIL_HOST_PASSWORD = 'qmhzfspdbtjdrvjc'
+EMAIL_HOST_USER = 'mohsenquera@gmail.com'
+EMAIL_HOST_PASSWORD = 'qcspmukrhdhgldem'
