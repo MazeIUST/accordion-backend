b8bb2a070426e1a9d39f129edfaa6e6799ee4e23
mhhasani <hasanimohammadhosein@gmail.com>
2022-11-04 12:41:03 +0330

    add pass1, pass2 and is_Artist to User

--- accordion/accounts/models.py
+++ accordion/accounts/models.py
@@ -2,7 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User, AbstractUser
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     is_email_verified = models.BooleanField(default=False)
+    is_Artist = models.BooleanField(default=False)
 
     def __str__(self):
         return self.email
+
+class Artist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
