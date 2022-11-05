4554c9775a5f9e0127be10a511ceb579928ba1c3
maryam1381 <74464095+maryam1381@users.noreply.github.com>
2022-11-05 16:49:10 +0330

    check mohsen git

--- accordion/accounts/models.py
+++ accordion/accounts/models.py
@@ -11,7 +11,6 @@ class User(AbstractUser):
 
 class Artist(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-
+    
     def __str__(self):
         return self.user.username
