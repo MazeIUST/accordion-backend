7a01ec64c9b018951e6ba7795cd5a88061a7f5d8
maryam1381 <74464095+maryam1381@users.noreply.github.com>
2022-11-14 08:09:45 +0330

    merge

--- accordion/accounts/models.py
+++ accordion/accounts/models.py
@@ -28,9 +28,9 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
-    
+
 class Artist(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return self.user.username
