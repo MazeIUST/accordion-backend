a785b600d307895dc83616bc0e52cca6034a795c
mhhasani <hasanimohammadhosein@gmail.com>
2022-11-23 16:15:23 +0330

    edit list

--- accordion/song/urls.py
+++ accordion/song/urls.py
@@ -4,9 +4,6 @@ from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
-router.register(r'admin', SongViewSet_Admin, basename='admin')
-router.register(r'artist', SongViewSet_Artist, basename='artist')
-router.register(r'user', SongViewSet_User, basename='user')
 router.register(r'', SongViewSet, basename='songs')
 router.register(r'tag', TagViewSet, basename='tag')
 
@@ -17,19 +14,6 @@ urlpatterns = [
     path('delete_all/', SongViewSet.as_view({'delete': 'delete_all'})),
     path('search/<str:text>/', SongViewSet.as_view({'get': 'search'}), name='song_search'),
 
-    path('admin/', SongViewSet_Admin.as_view({'get': 'list'})),
-    path('admin/<int:pk>/', SongViewSet_Admin.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-    path('admin/delete_all/', SongViewSet_Admin.as_view({'delete': 'destroy_all'})),
-
-    path('artist/', SongViewSet_Artist.as_view({'get': 'list', 'post': 'create'})),
-    path('artist/<int:pk>/', SongViewSet_Artist.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-    path('artist/zahra/', ZahraViewSet.as_view({'post': 'create'}), name='zahra'),
-
-    path('user/', SongViewSet_User.as_view({'get': 'list'}), name='songs'),
-    path('user/<int:pk>/', SongViewSet_User.as_view({'get': 'retrieve'}), name='song'),
-    path('user/search/<str:text>/', SongViewSet_User.as_view({'get': 'search'}), name='song_search'),
-
     path('tag/', TagViewSet.as_view({'get': 'list', 'post': 'create'}), name='tags'),
     path('tag/<int:pk>/', TagViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='tag'),
-
 ]
--- accordion/song/views.py
+++ accordion/song/views.py
@@ -11,85 +11,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 
 
-class SongViewSet_Admin(ViewSet):
-    serializer_class = SongSerializer
-    permission_classes = [IsSuperUser]
-
-    def list(self, request):
-        queryset = Song.objects.all()
-        serializer = SongSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        queryset = Song.objects.all()
-        song = get_object_or_404(queryset, pk=pk)
-        serializer = SongSerializer(song)
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        queryset = Song.objects.all()
-        song = get_object_or_404(queryset, pk=pk)
-        serializer = SongSerializer(song, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        queryset = Song.objects.all()
-        song = get_object_or_404(queryset, pk=pk)
-        song.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def destroy_all(self, request):
-        queryset = Song.objects.all()
-        queryset.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-class SongViewSet_Artist(ViewSet):
-    serializer_class = SongSerializer
-    permission_classes = [IsArtist]
-
-    def list(self, request):
-        artist = Artist.objects.get(user=request.user)
-        songs = Song.objects.filter(artist=artist)
-        serializer = SongSerializer(songs, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = SongSerializer(data=request.data)
-        if serializer.is_valid():
-            artist = Artist.objects.get(user=request.user)
-            serializer.save(artist=artist)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, pk=None):
-        try:
-            song = Song.objects.get(id=pk)
-            serializer = SongSerializer(song)
-            return Response(serializer.data)
-        except Song.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def update(self, request, pk=None):
-        song = Song.objects.get(id=pk)
-        serializer = SongSerializer(instance=song, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        song = Song.objects.filter(id=pk)
-        if song.exists():
-            song = song.first()
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        song.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
 class ZahraViewSet(ViewSet):
     serializer_class = SongSerializer
     authentication_classes = []
@@ -101,58 +22,7 @@ class ZahraViewSet(ViewSet):
             artist = Artist.objects.get(user__id = 1)
             serializer.save(artist=artist)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
-
-
-class SongViewSet_User(ViewSet):
-    serializer_class = SongSerializer
-    authentication_classes = []
-    permission_classes = []
-
-    def list(self, request):
-        songs = Song.objects.all()
-        if len(songs) > 15:
-            songs = songs[:15]
-    
-        serializer = SongSerializer(songs, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        try:
-            song = Song.objects.get(id=pk)
-            serializer = SongSerializer(song)
-            return Response(serializer.data)
-        except Song.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def search(self, request, text=None):
-        scores = {}
-        title_contains = Song.objects.filter(title__contains=text)
-        artist_contains = Song.objects.filter(Q(artist__user__first_name__contains=text) | Q(artist__user__last_name__contains=text))
-        lyrics_contains = Song.objects.filter(lyrics__contains=text)
-        
-        for song in title_contains:
-            scores[song] = 10
-        for song in artist_contains:
-            scores[song] = 5 if song not in scores else scores[song] + 5
-        for song in lyrics_contains:
-            scores[song] = 3 if song not in scores else scores[song] + 3
-
-        # save songs in a queryset
-        songs = []
-        for song in scores:
-            songs.append(song)
-
-        # sort songs by score
-        songs.sort(key=lambda x: scores[x], reverse=True)
-
-        # return top 15 songs   
-        if len(songs) > 15:
-            songs = songs[:15]
-
-        serializer = SongSerializer(songs, many=True)
-        return Response(serializer.data)
-            
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)         
 
 class SongViewSet(ViewSet):
     serializer_class = SongSerializer
@@ -169,11 +39,11 @@ class SongViewSet(ViewSet):
 
 
     def list(self, request):
-        if request.user.is_superuser:
-            songs = Song.objects.all()
-        else:
+        if request.user.is_Artist:
             artist = Artist.objects.get(user=request.user)
             songs = Song.objects.filter(artist=artist)
+        else:
+            songs = Song.objects.all()
         serializer = SongSerializer(songs, many=True)
         return Response(serializer.data)
 
