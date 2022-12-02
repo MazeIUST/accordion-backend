09cbc512cade8e0ecfb442e77d6dc0ab79f79f6a
mhhasani <hasanimohammadhosein@gmail.com>
2022-12-02 15:32:37 +0330

    song_download_link

--- accordion/song/urls.py
+++ accordion/song/urls.py
@@ -18,8 +18,6 @@ urlpatterns = [
     path('tag/', TagViewSet.as_view({'get': 'list', 'post': 'create'}), name='tags'),
     path('tag/<int:pk>/', TagViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='tag'),
 
-
-
     path('playlist/', PlaylistViewSet.as_view({'post': 'create', 'get': 'list'}), name='playlist'),
     path('playlist/<int:pk>/', PlaylistViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='playlist'),
     path('playlist/add_song/song_id/<int:song_pk>/playlist_id/<int:playlist_pk>/', PlaylistViewSet.as_view({'put': 'add_song'}), name='add_song'),
