9653e1e6a6ec3195e0019e4b3f4bfd52dc90e504
maryam1381 <74464095+maryam1381@users.noreply.github.com>
2022-12-11 23:11:56 +0330

    add city analysis

--- accordion/history/models.py
+++ accordion/history/models.py
@@ -8,6 +8,7 @@ class History(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
     add_datetime = models.DateTimeField(auto_now_add=True)
-    # location 
+
+    
 
 
--- accordion/history/views.py
+++ accordion/history/views.py
@@ -54,7 +54,51 @@ class HistoryViewSet(ViewSet):
                 result[tag.id]=+1
         return Response(result)
 
-    def get_city(self,request):
+    def get_city_last_day(self,request):
         users = User.objects.all().filter(city=request.data.get('city'))
-
+        today = datetime.datetime.now()
+        yesterday = today-datetime.timedelta(days=1)
+        users_history =History.objects.filter(add_datetime__range=[yesterday,today],user__in = users)
+        songs_tags = users_history.annotate(tags=F("song_id__tags")).values()
+        tags = Tag.objects.all()
+        result = {}
+        for tag in tags:
+            result[tag.id]=0
+        for song_tag in songs_tags:
+            for tag in song_tag.tags:
+                result[tag.id]=+1
+        return Response(result)
         # songs = Song.objects.filter(id__in=played_songs_id).values
+
+
+
+    def get_city_last_week(self,request):
+        users = User.objects.all().filter(city=request.data.get('city'))
+        today = datetime.datetime.now()
+        last_week = today-datetime.timedelta(weeks=1)
+        users_history =History.objects.filter(add_datetime__range=[last_week,today],user__in = users)
+        songs_tags = users_history.annotate(tags=F("song_id__tags")).values()
+        tags = Tag.objects.all()
+        result = {}
+        for tag in tags:
+            result[tag.id]=0
+        for song_tag in songs_tags:
+            for tag in song_tag.tags:
+                result[tag.id]=+1
+        return Response(result)
+    
+
+    def get_city_last_month(self,request):
+        users = User.objects.all().filter(city=request.data.get('city'))
+        today = datetime.datetime.now()
+        last_month = today-datetime.timedelta(days=30)
+        users_history =History.objects.filter(add_datetime__range=[last_month,today],user__in = users)
+        songs_tags = users_history.annotate(tags=F("song_id__tags")).values()
+        tags = Tag.objects.all()
+        result = {}
+        for tag in tags:
+            result[tag.id]=0
+        for song_tag in songs_tags:
+            for tag in song_tag.tags:
+                result[tag.id]=+1
+        return Response(result)
