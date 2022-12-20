8efa26c294ea4f56f7d34d695aaf0afa438d6661
mhhasani <hasanimohammadhosein@gmail.com>
2022-12-20 10:03:00 +0330

    permium in profile

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import  serializers
+from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
 from accounts.models import *
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -6,13 +6,17 @@ from song.serializers import *
 from datetime import datetime, timedelta
 
 # Register serializer
+
+
 class SignUpSerializer(serializers.ModelSerializer):
     password1 = serializers.CharField(write_only=True)
     password2 = serializers.CharField(write_only=True)
     is_Artist = serializers.BooleanField(required=False)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password1', 'password2', 'is_Artist')
+        fields = ('id', 'username', 'email',
+                  'password1', 'password2', 'is_Artist')
         extra_kwargs = {
             'password1': {'write_only': True},
             'password1': {'write_only': True},
@@ -40,27 +44,31 @@ class SignUpSerializer(serializers.ModelSerializer):
 
 class FollowerSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user1.username', read_only=True)
-    profile = serializers.HyperlinkedRelatedField(source='user1', read_only=True, view_name='show_other_user_profile')
-    
+    profile = serializers.HyperlinkedRelatedField(
+        source='user1', read_only=True, view_name='show_other_user_profile')
+
     class Meta:
         model = Follow
         fields = ('username', 'profile')
-        
-        
+
+
 class FollowingSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user2.username', read_only=True)
-    profile = serializers.HyperlinkedRelatedField(source='user2', read_only=True, view_name='show_other_user_profile')
-    
+    profile = serializers.HyperlinkedRelatedField(
+        source='user2', read_only=True, view_name='show_other_user_profile')
+
     class Meta:
         model = Follow
         fields = ('username', 'profile')
-        
-        
+
+
 class ArtistSerializer(serializers.ModelSerializer):
     songs = serializers.SerializerMethodField()
+
     class Meta:
         model = Artist
-        fields = ['artistic_name', 'activitie_start_date', 'description', 'songs']
+        fields = ['artistic_name', 'activitie_start_date',
+                  'description', 'songs']
 
     def get_songs(self, obj):
         songs = Song.objects.filter(artist__user=obj.user)
@@ -73,41 +81,50 @@ class UserSerializer(serializers.ModelSerializer):
     followings_count = serializers.SerializerMethodField()
     followers = serializers.SerializerMethodField()
     followings = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['username', 'is_Artist', 'first_name', 'last_name', 'image', 'bio', 'followers_count', 'followings_count', 'followers', 'followings', 'artist'] # should add playlist there
-        read_only_fields = ['id', 'username', 'email']  
-        
+        fields = ['username', 'is_Artist', 'first_name', 'last_name', 'image', 'bio', 'followers_count',
+                  'followings_count', 'followers', 'followings', 'artist']  # should add playlist there
+        read_only_fields = ['id', 'username', 'email']
+
     def get_followers_count(self, obj):
         followers_count = Follow.objects.filter(user2=obj).count()
         return followers_count
-    
+
     def get_followings_count(self, obj):
         followings_count = Follow.objects.filter(user1=obj).count()
         return followings_count
-    
+
     def get_followers(self, obj):
         followers = Follow.objects.filter(user2=obj)
         return FollowerSerializer(followers, many=True, context={'request': self.context['request']}).data
-    
+
     def get_followings(self, obj):
         followings = Follow.objects.filter(user1=obj)
         return FollowingSerializer(followings, many=True, context={'request': self.context['request']}).data
-    
 
 
 class ArtistPrivateSerializer(ArtistSerializer):
     class Meta(ArtistSerializer.Meta):
         fields = ['id'] + ArtistSerializer.Meta.fields
         read_only_fields = ['id']
-        
+
 
 class UserPrivateSerializer(UserSerializer):
     artist = ArtistPrivateSerializer()
-    
+    permium = serializers.SerializerMethodField()
+
     class Meta(UserSerializer.Meta):
-        fields = ['id', 'email', 'is_email_verified', 'telegram_chat_id', 'birthday', 'gender', 'country', 'city', 'bio', 'money'] + UserSerializer.Meta.fields
-        read_only_fields = ['id', 'username', 'is_Artist', 'email', 'is_email_verified', 'telegram_chat_id', 'money']      
+        fields = ['id', 'email', 'is_email_verified', 'telegram_chat_id', 'birthday',
+                  'gender', 'country', 'city', 'bio', 'money', 'permium'] + UserSerializer.Meta.fields
+        read_only_fields = ['id', 'username', 'is_Artist', 'email',
+                            'is_email_verified', 'telegram_chat_id', 'money', 'permium']
+
+    def get_permium(self, obj):
+        permium = Permium.objects.filter(
+            payment__user=obj, end_date__gte=datetime.now())
+        return PermiumSerializer(permium, many=True, context={'request': self.context['request']}).data
 
     def update(self, instance, validated_data):
         try:
@@ -121,15 +138,23 @@ class UserPrivateSerializer(UserSerializer):
 
 class PermiumSerializer(serializers.ModelSerializer):
     days = serializers.IntegerField(write_only=True)
+    payment = serializers.SerializerMethodField()
+
     class Meta:
         model = Permium
-        fields = ['id', 'start_date', 'end_date', 'is_active', 'days_left', 'days', 'payment']
-        read_only_fields = ['id', 'start_date', 'end_date', 'days_left', 'is_active', 'payment']
+        fields = ['id', 'start_date', 'end_date',
+                  'is_active', 'days_left', 'days', 'payment']
+        read_only_fields = ['id', 'start_date', 'end_date',
+                            'days_left', 'is_active', 'payment']
 
+    def get_payment(self, obj):
+        payment = Payment.objects.filter(pk=obj.payment.pk)
+        return PaymentSerializer(payment, many=True, context={'request': self.context['request']}).data
 
     def validate(self, attrs):
         user = self.context['request'].user
-        active_permium = Permium.objects.filter(payment__user=user, end_date__gte=datetime.now())
+        active_permium = Permium.objects.filter(
+            payment__user=user, end_date__gte=datetime.now())
 
         if attrs['days'] < 1:
             raise serializers.ValidationError({
@@ -139,13 +164,14 @@ class PermiumSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({
                 'user': ['user already has active permium.'],
             })
-        
-        return attrs  
+
+        return attrs
 
     def create(self, validated_data):
         days = validated_data.pop('days')
         validated_data['end_date'] = datetime.now() + timedelta(days=days)
         return super().create(validated_data)
+
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -160,7 +186,7 @@ class PaymentSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({
                 'amount': [f'user does not have enough money. your money is {user_money}.'],
             })
-        
+
         return attrs
 
 
@@ -181,6 +207,7 @@ class ChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
     new_password1 = serializers.CharField(required=True)
     new_password2 = serializers.CharField(required=True)
+
     class Meta:
         model = User
         fields = ('old_password', 'new_password1', 'new_password2')
@@ -205,4 +232,3 @@ class ChangePasswordSerializer(serializers.Serializer):
             raise serializers.ValidationError({
                 'old_password': ['Old password is incorrect.'],
             })
-            
