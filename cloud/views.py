0376ab5afd5676d68fb1e9db44da5dec870d40bc
mhhasani <hasanimohammadhosein@gmail.com>
2023-01-20 22:51:05 +0330

    serializers.ModelSerializer

--- cloud/views.py
+++ cloud/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from .models import Song
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from .serializers import SongSerializer
@@ -10,7 +9,7 @@ class SongViewSet(ViewSet):
     permission_classes = []
 
     def create(self, request):
-        serializer = self.serializer_class(
+        serializer = SongSerializer(
             data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
