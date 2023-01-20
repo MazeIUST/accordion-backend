d108589ecf33530990bea8b87171bde0305b9fa9
mhhasani <hasanimohammadhosein@gmail.com>
2023-01-20 20:45:04 +0330

    upload_to_drive

--- bot/main_funcs.py
+++ bot/main_funcs.py
@@ -19,6 +19,9 @@ import os
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
+from Google import Create_Service
+from googleapiclient.http import MediaFileUpload
+
 
 def send_request(url, options):
     url = SERVER_URL + url + '/'
@@ -108,7 +111,7 @@ def download_song(song_link):
     return song.name
 
 
-def upload_to_drive(song, context: CallbackContext):
+def upload_to_drive2(song, context: CallbackContext):
     # download song from telegram
     current_dir = os.path.dirname(os.path.abspath(__file__))
     songs_dir = os.path.join(current_dir, 'songs')
@@ -132,3 +135,40 @@ def upload_to_drive(song, context: CallbackContext):
     # id = 'sdlkfjslkdfjlsjdflk'
     song_link = f'https://drive.google.com/file/d/{id}/view?usp=drivesdk'
     return song_link
+
+
+def upload_to_drive(song, context: CallbackContext):
+    # download song from telegram
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    songs_dir = os.path.join(current_dir, 'songs')
+    if not os.path.exists(songs_dir):
+        os.mkdir(songs_dir)
+    song_path = os.path.join(songs_dir, 'song.mp3')
+    song_file = context.bot.get_file(song)
+    song_file.download(song_path)
+
+    # upload to drive
+    CLIENT_SECRET_FILE = 'client_secrets.json'
+    API_NAME = 'drive'
+    API_VERSION = 'v3'
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+    
+    folder_id = '1niLMRkf31NyU7rbtCznPoln03zsxj4Kr'
+    file_metadata = {
+        'name': song.title,
+        'parents': [folder_id]
+    }
+    media = MediaFileUpload(song_path, mimetype='audio/mp3')
+    file = service.files().create(
+        body=file_metadata,
+        media_body=media,
+        fields='id'
+    ).execute()
+    id = file.get('id')
+    song_link = f'https://drive.google.com/file/d/{id}/view?usp=drivesdk'
+    return song_link
+    
+    
+    
